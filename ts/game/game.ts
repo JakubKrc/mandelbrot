a1a81915e1477df064406b8e5c8bf0ff27c0c74e
krc.jakub <krc.jakub@gmail.com>
2022-12-29 22:28:53 +0100

    ffdsf

--- ts/game/game.ts
+++ ts/game/game.ts
@@ -1,29 +1,109 @@
 let gravity = 0.6;
 let obj:any = [];
 let selectedObject = 0;
-//let camera = new cameraClass(0,0);
 
-let fps = 60;
+let fps = 5;
 let mainInterval = true;
 
-let mierkaZvacsenia = 50;
+let hlavneCislo:complexImaginary = {
+    imaginary : 0,
+    real : 0
+};
+let cCislo:complexImaginary= {
+    imaginary:0,
+    real : 0
+}
+
+let mierkaZvacsenia = 150;
+let os = {
+    x: 0,
+    y: 0
+};
+
+let hlavnyImage = [];
   
 window.onload = function () {
+
+    document.getElementById("canvas").addEventListener("contextmenu", function(event){
+        event.preventDefault();
+    });
 
     inicializeKeyboard();
     inicializeCanvas("canvas");
     inicializeMouse();
 
+    for(let i=0;i<canvas.width;i++){
+        hlavnyImage[i] = new Array();
+    }
+
+    os.x = canvas.width/2;
+    os.y = canvas.height/2;
+
+    for(let x=-2;x<2;x+=0.01)
+            for(let y=-2;y<2;y+=0.01)
+                if (isStable({real:x,imaginary:y}))
+                    hlavnyImage[Math.round(os.x + x * mierkaZvacsenia)][Math.round(os.y + y * mierkaZvacsenia)] = true;
+
     needFullscreenToRun = false;   // toto su globalne premenne, ked to bude hra, tak to bude vyzadovat fullscreen a lockmouse
     needMouseLockToRun = false;     // tiez neviem uplne preco, ale tie sa uplne odporucaju nepouzivat
                                     // lepsie vytvorit triedu gameEngine? kde to bude vsetko?
             
-    //setInterval(mainCalculate ,1/fps);       //toto bude treba prerobit, nejde to rovanko rychlo na vsetkych kompoch, je to na youtube
-    mainDraw();        
+    setInterval(mainCalculate ,1000/fps);       //toto bude treba prerobit, nejde to rovanko rychlo na vsetkych kompoch, je to na youtube
+    setInterval(mainDraw ,1000/fps);         
 
 }
 
-/*function mainCalculate():void {
+type complexImaginary = {real:number,imaginary:number};
+
+function squareComplexImaginary(z:complexImaginary){
+    return {real : z.real * z.real - z.imaginary * z.imaginary,
+            imaginary : 2*z.real*z.imaginary}
+}
+
+function addComplexImaginary(x:complexImaginary,y:complexImaginary){
+    return {real: x.real+y.real, imaginary: x.imaginary+y.imaginary};
+}
+
+function mandel(z:complexImaginary, c:complexImaginary):complexImaginary{
+    return addComplexImaginary (squareComplexImaginary(z), c);
+}
+
+function isStable(z:complexImaginary):boolean{
+    let docasne:complexImaginary = {
+        real:z.real,
+        imaginary:z.imaginary
+    }
+
+    for(let i=0; i<10; i++){
+        if(docasne.real < -2 || docasne.real > 2 || docasne.imaginary < -2 || docasne.imaginary > 2)
+            return false;
+        docasne = mandel(docasne, cCislo);
+    }
+
+    return true;
+}
+
+function opakovaneVykreslenie (z:complexImaginary){
+    let docasne:complexImaginary = {
+        real:z.real,
+        imaginary:z.imaginary
+    }
+
+    canvasCtx.beginPath();
+    canvasCtx.moveTo(os.x + docasne.real * mierkaZvacsenia,os.y + docasne.imaginary * mierkaZvacsenia);
+
+    for(let i=0; i<13; i++){
+        canvasCtx.fillRect(os.x + docasne.real * mierkaZvacsenia,os.y + docasne.imaginary * mierkaZvacsenia ,canvas.width/80, canvas.height/80);
+        docasne = mandel(docasne, cCislo);
+        canvasCtx.lineTo(os.x + docasne.real * mierkaZvacsenia,os.y + docasne.imaginary * mierkaZvacsenia);
+    }
+
+    canvasCtx.stroke();
+
+
+}
+
+function mainCalculate():void {
 
     if( keyPressedWaitForKeyUp ( keys['menu'] ) )     //pauzovanie. velmi som spokojny s jednoduchostou. v prvej verzii kubohry sa to ukazalo ako
                                                         //prekvapivy problem dorobit
@@ -31,84 +111,58 @@ window.onload = function () {
 
     if (!mainInterval) return;                          //rozdelene vykreslovanie a pocitanie pohybu a vsetkeho. teraz sa nic nepohne,
                                                         //ale stale mozes obnovit obraz. vyhoda pri tvorbe menu, ako som zistil minule
-
-   /* if(keyPressed(keys['up'])) obj[selectedObject].addVector(Math.PI/2, 0.3);      //cacane, takyto zapis sa mi paci. co sa skryva podtymi funkciami je uz
-    if(keyPressed(keys['down'])) obj[selectedObject].addVector(3*Math.PI/2, 0.3);   //asi horsie. controls.ts a physics.ts
-    if(keyPressed(keys['left'])) obj[selectedObject].addVector(Math.PI, 0.3);
-    if(keyPressed(keys['right'])) obj[selectedObject].addVector(2*Math.PI, 0.3);*/
     
-    /*if(keyPressedWaitForKeyUp(keys['fire'])) {
-        let x = obj.push(new playableObj(crosshair.x, crosshair.y,6,6, true, false));  //totok bude spojene s gifkom a nie velkostou stvorca
-        obj[x-1].addVector(crosshair.aimingAngle, 8);                                  //tiez som chcel mat zvlast projektil class,
-    }                                                                                  //a vobec tie class sa mi moc nepacia, neviem to v tom
-                                                                                       //zmysluplne rozdelit. rozmyslam na to skoro uplne ich 
-    if(keyPressedWaitForKeyUp(keys['q'])) selectedObject++                             //vypustit a rozdelit data a funkcie. vyhoda aj pri savovani
-    if(keyPressedWaitForKeyUp(keys['e'])) selectedObject--
-    selectedObject = Math.max(0, Math.min(selectedObject, obj.length - 1)) */  //moznost ovladat iny objekt, to v tej hre chcem mat, toto je len test, ktory to zlbne
-
-   /* eventsRun();            //spravi krok v eventoch
-    
-}*/
-
-function mandel(f:number, c:number):number{
-    return (f*f) + c;
-}
-
-function mandelVybuchCiNie(f:number, mandelN:number):number{
-    let kolkoKratOpakovatMandla = 7;
-    let pametajPoslednehoMandla = mandel(f, mandelN);
-
-    for (let i=0; i<kolkoKratOpakovatMandla; i++){
-        pametajPoslednehoMandla = mandel(f,pametajPoslednehoMandla);
+    if(keyPressed(keys['fire'])) {
+        hlavneCislo.real = (mousePos.x - os.x)/mierkaZvacsenia;
+        hlavneCislo.imaginary = (mousePos.y - os.y)/mierkaZvacsenia;
+    } else {
+        hlavneCislo.real = 0;
+        hlavneCislo.imaginary = 0;
     }
 
-    return pametajPoslednehoMandla;
-}
-
-function mandelOpakovaneKreslenie(f:number, mandelN:number){
-    let kolkoKratOpakovatMandla = 150;
-    let vysledokFunkcie = mandel(f, mandelN);
-
-    for (let i=0; i<kolkoKratOpakovatMandla; i++){
-        vysledokFunkcie = mandel(f,vysledokFunkcie);
-        canvasCtx.fillRect(100 + f*mierkaZvacsenia ,100 + vysledokFunkcie*mierkaZvacsenia,1,1);
-        console.log();
+    if(keyPressed(keys['use'])){
+        cCislo.real = (mousePos.x - os.x)/mierkaZvacsenia;
+        cCislo.imaginary = (mousePos.y - os.y)/mierkaZvacsenia;
+        for(let x=-2;x<2;x+=0.01)
+            for(let y=-2;y<2;y+=0.01)
+                    hlavnyImage[Math.round(os.x + x * mierkaZvacsenia)][Math.round(os.y + y * mierkaZvacsenia)] = false;
+        for(let x=-2;x<2;x+=0.01)
+            for(let y=-2;y<2;y+=0.01)
+                if (isStable({real:x,imaginary:y}))
+                    hlavnyImage[Math.round(os.x + x * mierkaZvacsenia)][Math.round(os.y + y * mierkaZvacsenia)] = true;
     }
+    
+   // eventsRun();            //spravi krok v eventoch
+    
 }
 
 function mainDraw(){
 
     canvasCtx.fillStyle='green';
-    canvasCtx.fillRect(0,0,canvas.width,canvas.height)		
+    canvasCtx.fillRect(0,0,canvas.width,canvas.height);
 
-  /*  if(mouseInicialized){
-        canvasCtx.fillStyle='yellow';
-        canvasCtx.fillRect(mousePos.x ,mousePos.y,6,6);	//kresli mysku
-    }*/
-
-    let vysledokFunkcie:number;
-
-    let c=0;
-
-    canvasCtx.fillStyle='red';
-   /* for(let f=-1; f<1; f+=0.01)
-      /*  for(let c=-1; c<1; c+=0.01)*//*{
-            vysledokFunkcie = mandelVybuchCiNie(f,c);
-
-            if(vysledokFunkcie < 1 && vysledokFunkcie > -1)
-                /*canvasCtx.fillRect(300 + f*mierkaZvacsenia ,300 + vysledokFunkcie,1,1); */
-              //  mandelOpakovaneKreslenie(f,c);
-   //     }*/
-
-   mandelOpakovaneKreslenie(0.4,0.1);
+    canvasCtx.fillStyle='orange';
+    for(let x=0;x<canvas.width;x+=1)
+        for(let y=0;y<canvas.height;y+=1)
+            if (hlavnyImage[x][y])
+                canvasCtx.fillRect(x,y,2, 2);
 
     canvasCtx.fillStyle='blue';
-    canvasCtx.fillRect(100 ,100,6,6);
+    canvasCtx.fillRect(0, os.y , canvas.width , canvas.height/200);
+    canvasCtx.fillRect(os.x, 0 , canvas.width/200, canvas.height);
 
-    fullscreenLockmouseMsg();   // ta upozornuje, ked ta to nuti zalokovat mys a dat do fulscreenu. teraz je to vypnute, ale ked to bude
-                                //vonku, tak sa to mas hrat jak na 486ke, nie v prehliadaci. v tom to je len kvoli dostupnosti.
-                                //ale moznost vykreslovat sirokouhlo to mat bude, hoci asi to nebude standardne nastavenie, neviem este
-                                //vlasne aj toto sa bude dat vypnut, ale vynada ti to pri tom
-   // requestAnimationFrame(mainDraw);   //daky novy sposob vykreslovania na canvas. nemusis riesit fps ani sync s monitorom. rad som vyuzil
+    if(hlavneCislo.real!=0 && hlavneCislo.imaginary!=0){
+        canvasCtx.fillStyle='red';
+        opakovaneVykreslenie(hlavneCislo);
+    }
+
+    if(mouseInicialized){
+        canvasCtx.fillStyle='yellow';
+        canvasCtx.fillRect(mousePos.x ,mousePos.y,6,6);
+    }
+
+    fullscreenLockmouseMsg();  
+
+    //requestAnimationFrame(mainDraw);   //daky novy sposob vykreslovania na canvas. nemusis riesit fps ani sync s monitorom. rad som vyuzil
 
 }
